f02fe8bf998014a73b35ae4b8829bf7597b72e93
Salvatore Polizzotti <fenrhir@libero.it>
2024-02-27 08:40:45 +0100

    now button is disabled when counter is <=than 0

--- src/components/BtnDecrement.tsx
+++ src/components/BtnDecrement.tsx
@@ -1,7 +1,12 @@
 import { useContext } from "react";
 import { ContextApp } from "../context/Context";
+import { Counter } from "./Counter";
 
 export const BtnDecrement = () => {
-  const { onClickDecrement } = useContext(ContextApp);
-  return <button onClick={onClickDecrement}>Decrement</button>;
+  const { onClickDecrement, counter } = useContext(ContextApp);
+  return (
+    <button disabled={counter <= 0 ? true : false} onClick={onClickDecrement}>
+      Decrement
+    </button>
+  );
 };
